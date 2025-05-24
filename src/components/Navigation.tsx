2c77abcad23f4f0bf884bd8f7c9405e6dff9e7c4
Yassine Elmellouki <yelmellouki@gmail.com>
2025-05-23 20:13:00 -0400

    slight changes

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import givinLogo from '../assets/givin-logo.png';
+import givinLogo from '../assets/givin-logo.svg';
 import '../styles/theme.css';
 import './Navigation.css';
 
